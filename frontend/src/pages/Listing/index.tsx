224b7c4331a7bdc9556d2371048a6421b434b99f
VicenteLamberti <vicente.lamberti@hotmail.com>
2022-03-12 16:04:32 -0300

    useEffect,useState

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -5,16 +5,35 @@ import "./styles.css"
 import MovieCard from "components/MovieCard";
 import axios from "axios";
 import { BASE_URL } from "utils/requests";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 
 function Listing() {
 
-    //FORMA ERRADA
-    axios.get(`${BASE_URL}/movies?size=12&page=0`).then(response =>{
-        console.log(response.data);
-    });
+    const [pageNumber, setPageNumber] = useState(0);
+
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=1`).then(response => {
+            console.log(response.data);
+            const data = response.data as MoviePage;
+            console.log(data)
+            setPageNumber(data.number);
+        });
+    }, []);
+
+    // //FORMA ERRADA
+    // axios.get(`${BASE_URL}/movies?size=12&page=1`).then(response =>{
+    //     console.log(response.data);
+    //     const data = response.data as MoviePage;
+    //     setPageNumber(data.number);
+    // });
+
+
 
     return (
         <>
+            <p>{pageNumber}</p>
             <Pagination />
             <div className="container">
                 <div className='row'>
@@ -37,10 +56,10 @@ function Listing() {
                     <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div>
-                
+
                 </div>
 
-                
+
 
 
             </div>
